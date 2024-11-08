c19c9dd07f9fd863d1b59c6f29a96e12bccfa039
Anusha-star11 <anushapoosala11@gmail.com>
2024-11-08 18:54:08 +0530

    mmmm

--- RealEstate/vite.config.js
+++ RealEstate/vite.config.js
@@ -1,9 +1,22 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  // Remove the base config since you're serving from root
   build: {
-    outDir: 'dist', // Ensure the build output goes to the dist folder
+    rollupOptions: {
+      output: {
+        entryFileNames: '[name].[hash].js',
+        chunkFileNames: '[name].[hash].js',
+        assetFileNames: '[name].[hash][extname]'
+      }
+    }
   },
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': 'http://localhost:5001'
+    }
+  }
 })
