9c07975b2d94c4bfd53882e2a40f35d14cf1db79
Anusha-star11 <anushapoosala11@gmail.com>
2024-11-08 11:33:02 +0530

    lllll

--- RealEstate/src/main.jsx
+++ RealEstate/src/main.jsx
@@ -8,6 +8,19 @@ import './index.css'
 // Configure Font Awesome
 config.autoAddCss = false
 
+// Add meta tag for CSP
+const meta = document.createElement('meta');
+meta.httpEquiv = "Content-Security-Policy";
+meta.content = `
+  default-src 'self';
+  font-src 'self' data: https: https://fonts.gstatic.com https://ka-f.fontawesome.com;
+  img-src 'self' data: https:;
+  style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
+  script-src 'self' 'unsafe-inline' 'unsafe-eval';
+  connect-src 'self' https://v9-properties.onrender.com https://ka-f.fontawesome.com;
+`;
+document.head.appendChild(meta);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
