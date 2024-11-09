dae808c8e1f6d3300e13c1c2dafea9d93bce72f6
Anusha-star11 <anushapoosala11@gmail.com>
2024-11-09 15:39:18 +0530

    rtrt

--- RealEstate/src/url.js
+++ RealEstate/src/url.js
@@ -1,4 +1,4 @@
 const baseURL = import.meta.env.PROD 
-  ? 'https://anusha-star11.github.io/RealEstateApp/'  // Production URL
+  ? 'https://realestateapp-tifp.onrender.com'  // Production URL
   : 'http://localhost:5001'; // For local development
 export default baseURL;
